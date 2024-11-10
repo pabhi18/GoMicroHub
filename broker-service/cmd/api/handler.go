5eb7200e1033b5e5e513413c0364a195f39d9877
Sanjay Gupta <sanjaygupta@Sanjays-MacBook-Air.local>
2024-11-11 03:51:22 +0530

    writting docker file for first broker service

--- broker-service/cmd/api/handler.go
+++ broker-service/cmd/api/handler.go
@@ -11,7 +11,7 @@ type jsonResponse struct {
 	Data    any    `json:"data,omitempty"`
 }
 
-func (app *Config) Broker(w http.ResponseWriter, r http.Request) {
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
 		Error:   false,
 		Message: "Hit the broker",
