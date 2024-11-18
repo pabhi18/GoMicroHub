ac012cf80e1a4c089187d1bc8ff421fc33d60671
Sanjay Gupta <sanjaygupta@Sanjays-MacBook-Air.local>
2024-11-19 00:07:48 +0530

    completed db connection of authentication service

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -6,9 +6,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 const webPort = "7070"
+
+var counts int64
 
 type Config struct {
 	DB     *sql.DB
@@ -20,8 +28,14 @@ func main() {
 
 	// connect db here
 
+	conn := connectDB()
+
+	app := Config{
+		DB:     conn,
+		models: data.New(conn),
+	}
+
 	// set config
-	app := Config{}
 	srv := http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
@@ -34,4 +48,38 @@ func main() {
 		panic(err)
 	}
 
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func connectDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not ready to connect ....")
+			counts++
+		} else {
+			log.Println("Connected to Postgres !")
+			return connection
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for two seconds")
+		time.Sleep(2 * time.Second)
+	}
 }
--- authentication-service/data/models.go
+++ authentication-service/data/models.go
@@ -13,8 +13,6 @@ const dbTimeout = time.Second * 3
 
 var db *sql.DB
 
-// New is the function used to create an instance of the data package. It returns the type
-// Model, which embeds all the types we want to be available to our application.
 func New(dbPool *sql.DB) Models {
 	db = dbPool
 
@@ -23,14 +21,10 @@ func New(dbPool *sql.DB) Models {
 	}
 }
 
-// Models is the type for this package. Note that any model that is included as a member
-// in this type is available to us throughout the application, anywhere that the
-// app variable is used, provided that the model is also added in the New function.
 type Models struct {
 	User User
 }
 
-// User is the structure which holds one user from the database.
 type User struct {
 	ID        int       `json:"id"`
 	Email     string    `json:"email"`
@@ -42,7 +36,6 @@ type User struct {
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
-// GetAll returns a slice of all users, sorted by last name
 func (u *User) GetAll() ([]*User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
@@ -81,7 +74,6 @@ func (u *User) GetAll() ([]*User, error) {
 	return users, nil
 }
 
-// GetByEmail returns one user by email
 func (u *User) GetByEmail(email string) (*User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
@@ -109,7 +101,6 @@ func (u *User) GetByEmail(email string) (*User, error) {
 	return &user, nil
 }
 
-// GetOne returns one user by id
 func (u *User) GetOne(id int) (*User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
@@ -137,8 +128,6 @@ func (u *User) GetOne(id int) (*User, error) {
 	return &user, nil
 }
 
-// Update updates one user in the database, using the information
-// stored in the receiver u
 func (u *User) Update() error {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
@@ -168,7 +157,6 @@ func (u *User) Update() error {
 	return nil
 }
 
-// Delete deletes one user from the database, by User.ID
 func (u *User) Delete() error {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
@@ -183,7 +171,6 @@ func (u *User) Delete() error {
 	return nil
 }
 
-// DeleteByID deletes one user from the database, by ID
 func (u *User) DeleteByID(id int) error {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
@@ -197,8 +184,6 @@ func (u *User) DeleteByID(id int) error {
 
 	return nil
 }
-
-// Insert inserts a new user into the database, and returns the ID of the newly inserted row
 func (u *User) Insert(user User) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
@@ -229,7 +214,6 @@ func (u *User) Insert(user User) (int, error) {
 	return newID, nil
 }
 
-// ResetPassword is the method we will use to change a user's password.
 func (u *User) ResetPassword(password string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
@@ -248,15 +232,11 @@ func (u *User) ResetPassword(password string) error {
 	return nil
 }
 
-// PasswordMatches uses Go's bcrypt package to compare a user supplied password
-// with the hash we have stored for a given user in the database. If the password
-// and hash match, we return true; otherwise, we return false.
 func (u *User) PasswordMatches(plainText string) (bool, error) {
 	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(plainText))
 	if err != nil {
 		switch {
 		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
-			// invalid password
 			return false, nil
 		default:
 			return false, err
