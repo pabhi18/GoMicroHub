9e71fd2dba96d6547f4cf43db49ee6fcee18e068
sanjaygupta972004 <sanjaygupta07054@gmail.com>
2024-11-25 03:47:11 +0530

    completed logger-service

--- authentication-service/cmd/api/hander.go
+++ authentication-service/cmd/api/hander.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -31,6 +33,13 @@ func (app *Config) Authentication(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// implement authentication service
+
+	err = app.logRequest("authentication", fmt.Sprintf("%s logged in", user.Email))
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+	}
+
 	payload := jsonResponse{
 		Error:   false,
 		Message: fmt.Sprintf("Logged in user %s", &user.Email),
@@ -39,4 +48,33 @@ func (app *Config) Authentication(w http.ResponseWriter, r *http.Request) {
 
 	app.writeJSON(w, payload, http.StatusOK)
 
+}
+
+func (app *Config) logRequest(name, data string) error {
+	var entry struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+	entry.Name = name
+	entry.Data = data
+
+	jsonData, _ := json.MarshalIndent(entry, "", "/t")
+
+	logServiceURL := "http://logger-service/log"
+
+	req, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	client := http.Client{}
+
+	response, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer response.Body.Close()
+
+	return nil
 }
